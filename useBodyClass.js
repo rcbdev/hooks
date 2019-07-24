d3ae80083a6137ecdd15f7fec334396ba1180311
Rob Burgess <rob.burgess@live.co.uk>
2019-07-24 14:19:43 +0100

    Update useBodyClass.js

--- useBodyClass.js
+++ useBodyClass.js
@@ -1,11 +1,11 @@
 import { useEffect } from 'react';
 
 const useBodyClass = className => {
-    useEffect(() => {
-        document.body.classList.add(className);
+  useEffect(() => {
+    document.body.classList.add(className);
 
-        return () => document.body.classList.remove(className);
-    }, [className]);
+    return () => document.body.classList.remove(className);
+  }, [className]);
 };
 
-export { useBodyClass };
+export default useBodyClass;
