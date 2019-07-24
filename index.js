c4bd7086625be75cfea6fd8157bf9b2538506521
Rob Burgess <rob.burgess@live.co.uk>
2019-07-24 21:00:10 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+export { default as useConsoleLog } from './useConsoleLog'
+export { default as useDocumentTitle } from './useDocumentTitle'
+export { default as useBodyClass } from './useBodyClass'
