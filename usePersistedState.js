361d2f6db51d6af680fcc3f7d58cfb1ed0acb85f
Rob Burgess <rob.burgess@live.co.uk>
2019-08-02 10:19:04 +0100

    Create usePersistedState.js

---
+++ usePersistedState.js
@@ -0,0 +1,42 @@
+import * as React from 'react';
+
+const { useState, useEffect } = React;
+
+const usePersistedState = (defaultState, key) => {
+    const [value, setValue] = useState(defaultState);
+
+    const setValueInStorage = x => {
+        const newValue = typeof x === "function"
+            ? x(value)
+            : x;
+        localStorage.setItem(key, JSON.stringify(newValue));
+        setValue(newValue);
+    };
+
+    useEffect(() => {
+        const restored = localStorage.getItem(key);
+        if (restored) {
+            setValue(JSON.parse(restored));
+        }
+    }, []);
+
+    useEffect(() => {
+        const handler = e => {
+            if (e.key === key) {
+                if (e.newValue) {
+                    setValue(JSON.parse(e.newValue));
+                } else {
+                    setValue(defaultState);
+                }
+            }
+        };
+
+        window.addEventListener("storage", handler);
+
+        return () => window.removeEventListener("storage", handler);
+    }, []);
+
+    return [value, setValueInStorage];
+};
+
+export default usePersistedState;
