8d5f4760f467fcd073efbf3cbab37502810c01b0
Rob Burgess <rob.burgess@live.co.uk>
2019-08-02 10:19:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-export { default as useConsoleLog } from './useConsoleLog'
-export { default as useDocumentTitle } from './useDocumentTitle'
-export { default as useBodyClass } from './useBodyClass'
-export { default as useDebounce } from './useDebounce'
+export { default as useConsoleLog } from './useConsoleLog';
+export { default as useDocumentTitle } from './useDocumentTitle';
+export { default as useBodyClass } from './useBodyClass';
+export { default as useDebounce } from './useDebounce';
+export { default as usePersistedState } from './usePersistedState';
